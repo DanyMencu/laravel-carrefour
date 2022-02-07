58fa6227c7e570c7c997a6b8497dd9642e1e87d0
DanyMencu <dany69men@gmail.com>
2022-02-07 16:15:56 +0100

    Project setup

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guests.welcome');
 });
+
+Auth::routes();
+
+//Admin route
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(
+        function () {
+            Route::get('/', 'HomeController@index')->name('home');
+    });
