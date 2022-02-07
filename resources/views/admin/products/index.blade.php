bca1f155f8b6d5c076dfb0cce2b219076718ad7d
abdi-El <abdila1099@gmail.com>
2022-02-07 18:09:18 +0100

    some changes

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -3,11 +3,32 @@
 @section('content')
     <div class="container">
         <div class="row justify-ccontent-center">
-            <div class="col-8">
+            <div class="col-10">
                 <table class="table">
                     <thead class="thead-dark">
-
-                    </thead>
+                        <tr>
+                            <th>name</th>
+                            <th>price</th>
+                            <th colspan="3">actions</th>
+                        </tr>
+                    </thead> 
+                    <tbody>
+                        @foreach($products as $product)
+                            <tr>
+                                <td>{{$product->name}}</td>
+                                <td>{{$product->name}}</td>
+                                <td>
+                                    <a href="{{route('admin.products.show', $product->slug)}}">see more</a>
+                                    <a href="{{route('admin.products.edit', $product->id)}}">edit product</a>
+                                    <form action="{{route('admin.products.destroy', $post->id)}}">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="btn btn-danger" on-click="return confirm('Are you sure you want to delete?')">delete product</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
                 </table>
             </div>
         </div>
