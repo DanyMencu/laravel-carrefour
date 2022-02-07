a5344581ee344b756d09121080a965cb207338ff
elepot87 <78643839+elepot87@users.noreply.github.com>
2022-02-07 21:58:58 +0100

    add content in show, add fontawesome

--- resources/views/admin/products/show.blade.php
+++ resources/views/admin/products/show.blade.php
@@ -1,5 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Pagina dettaglio</h1>
+<div class="container">
+    <div class="row">     
+        <h1 class="my-4">{{$product->name}}</h1>
+    </div>
+    <div class="row">
+        <div class="col-12 col-lg-6">
+            {{$product->description}}
+        </div>
+        <div class="col-12 col-lg-6">
+            image here
+        </div>
+    </div>  
+    <div class="row">
+        <div class="col-12">
+            <button type="button" class="btn btn-primary mt-4 me-3">
+                <a href="{{route('admin.products.create')}}" class="cta-create">Create new product</a>
+            </button>
+            <button type="button" class="btn btn-warning mt-4">
+                <a href="{{route('admin.products.edit', $product->id)}}" class="cta-edit">Edit {{$product->name}}</a>
+            </button>
+        </div>
+    </div>
+    <div class="row">
+        <a href="{{route('admin.products.index')}}" class="back-to mt-4">
+        <i class="fas fa-arrow-left icon-back-to"></i>
+        Back to Archive
+        </a>
+    </div> 
+</div>
 @endsection
