a66a6c606ffe2475f8095a21e3710b290814871c
abdi-El <abdila1099@gmail.com>
2022-02-08 22:48:22 +0100

    add CategoriesTableSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             ProductsTableSeeder::class,
-            AllergensTableSeeder::class,
+            CategoriesTableSeeder::class,
+            AllergensTableSeeder::class
         ]);
     }
 }
