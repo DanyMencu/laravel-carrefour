81196410116984440f7ca6317b5be0d96e894e01
jacopo-ferroni <jacopoferroni2706@gmail.com>
2022-02-09 17:04:02 +0100

    inserimento axios + popolamento pagina

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,28 +1,64 @@
 <template>
-    <div class="content">
-        <div class="title m-b-md">
-            Carrefour Front-office Site HERE
+    <div class="container">
+        <div class="box mb-4" v-for="product in products" :key="'product-${product.id}'">
+            <h1>{{product.name}}</h1>
+            <p>{{product.description}}</p>
+            <p>€ {{product.price}}</p>
+            <div class="cart">
+                <div class="btn">
+                    <i class="fa-solid fa-cart-shopping"></i>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name: "App",
     component: {},
+    data() {
+        return {
+            products : null,
+        }
+    },
+    created() {
+       this.getProducts();
+    },
+    methods : {
+        getProducts() {
+            const axios = require('axios');
+
+            // Make a request for a user with a given ID
+            axios.get('http://127.0.0.1:8005/api/products')
+            .then(response => {
+                // handle success
+                this.products = response.data;
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .then(function () {
+                // always executed
+            });
+
+        }
+    }
 }
 </script>
 
 <style lang="scss">
-    .content {
-        text-align: center;
-    }
-
-    .title {
-        font-size: 84px;
-    }
-
-    .m-b-md {
-        margin-bottom: 30px;
+.container {
+    max-width: 1100px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+}
+    .box {
+        width: calc(100% / 2);
     }
 </style>
