98c11b2a39ea194eb2c45675fedc7d3aee437421
DanyMencu <dany69men@gmail.com>
2022-02-15 15:26:04 +0100

    Install router and create Vue Header

---
+++ resources/js/pages/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container text-center my-5">
+        <h1 class="mb-4">ERROR 404</h1>
+
+        <p>Sorry, the page you are trying to access is not accessible... :(</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NotFound',
+}
+</script>
+
+<style>
+
+</style>
