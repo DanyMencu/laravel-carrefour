47348a5900bbdd1a20cb82ad8c7bb79fc256d370
Aurora Grippaudo <aurora268@hotmail.it>
2022-02-15 17:11:47 +0100

    Added props to card

--- resources/js/components/Card.vue
+++ resources/js/components/Card.vue
@@ -1,14 +1,24 @@
 <template>
-  <div class="card text-center">
+
+<section>
+  <div v-for="product in list"
+   :key="`product-${product.id}`"
+    class="card text-center">
+
       <h1>{{product.name}}</h1>
       <h1>{{product.description}}</h1>
       <h2>{{product.price}}</h2>
   </div>
+</section>
+
 </template>
 
 <script>
 export default {
-name:'Card'
+name:'Card',
+props:{
+  list:Array,
+}
 }
 </script>
 
