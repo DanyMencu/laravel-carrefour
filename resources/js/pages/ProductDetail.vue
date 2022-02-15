850ceca66e7413d2715cc540ddd2b87d656b2f17
elepot87 <eleonorapotente@gmail.com>
2022-02-15 15:28:16 +0100

    set up product detail frontend

---
+++ resources/js/pages/ProductDetail.vue
@@ -0,0 +1,24 @@
+<template></template>
+
+<script>
+import axios from "axios";
+
+export default {
+    name: "ProductDetail",
+    data() {
+        return {
+            product: null,
+        };
+    },
+    created() {
+        this.getProductDetail();
+    },
+    methods: {
+        getProductDetail() {
+            console.log("dettagli prodotto presi");
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
