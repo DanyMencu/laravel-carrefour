55d1f62aa65194152cb8a97410e159fad900da86
stef_fab@hotmail.it <stef_fab@hotmail.it>
2022-02-15 16:07:24 +0100

    add 2

---
+++ resources/js/pages/Contact.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <h1 class="mb-5">Conttataci</h1>
+        <section class="row">
+            <div class="col-md-6">
+                <form @submit.prevent="">
+                    
+                </form>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Contact',
+}
+</script>
+
+<style>
+
+</style>
