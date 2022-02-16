14264565539ef826475b36470e98914f0dfd16f1
DanyMencu <dany69men@gmail.com>
2022-02-16 15:39:33 +0100

    Add Loader and some images

--- resources/js/pages/Products.vue
+++ resources/js/pages/Products.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="products pt-3">
     <div class="container-fluid">
-        <div class="row">
+        <div v-if="products" class="row">
             <div class="col-2" v-for="(product, index) in products" :key='`product-${index}`'>
                 <div class='card-container mb-5'>
                     <Card :product="product"/>
                 </div>
             </div>
         </div>
+        <Loader v-else/>
     </div>
   </div>
 </template>
@@ -15,10 +16,13 @@
 <script>
 import axios from "axios"
 import Card from "../components/Card"
+import Loader from "../components/Loader";
+
 export default {
     name: 'products',
     components: {
         Card,
+        Loader,
     },
     data(){
         return({
